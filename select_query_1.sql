fc041ae58f6b437055a606eb2cdfe335bf609bb8
Tyler-Applegate <tyler.applegate@gmail.com>
2021-09-01 12:18:13 -0500

    completed easy, onto medium

--- select_query_1.sql
+++ select_query_1.sql
@@ -144,6 +144,19 @@ ORDER BY employee_id ASC;
 SELECT MAX(POPULATION) - MIN(POPULATION)
 FROM CITY;
 
+-- Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
+
+-- Equilateral: It's a triangle with  sides of equal length.
+-- Isosceles: It's a triangle with  sides of equal length.
+-- Scalene: It's a triangle with  sides of differing lengths.
+-- Not A Triangle: The given values of A, B, and C don't form a triangle.
+
+SELECT IF(
+A+B>C AND A+C>B AND B+C>A,
+IF(A=B AND B=C, 'Equilateral',
+  IF(A=B OR B=C OR A=C, 'Isosceles', 'Scalene')), 'Not A Triangle')
+  FROM TRIANGLES;
+
 -- Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, but did not realize her keyboard's  key was broken until after completing the calculation. She wants your help finding the difference between her miscalculation (using salaries with any zeros removed), and the actual average salary.
 
 -- Write a query calculating the amount of error (i.e.:  average monthly salaries), and round it up to the next integer.
